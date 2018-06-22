442fad1b41010cc5e793b734561a8a8ac5e647bb
Jay Causing <jac84@njit.edu>
2018-06-22 16:25:55 -0400

    updated ARCore to 1.3, starting utils library

---
+++ Assets/_PROJECT/Scripts/ARCoreUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+/*
+* Custom library for handling core ARCore functionality
+* 
+ */
+public class ARCoreUtils : MonoBehaviour {
+
+	public void TrackPlanes(List<DetectedPlane> l_Planes){
+
+	}
+
+	public void QuitSessionOnAppError(){
+
+	}
+
+	public void UpdateAppLifecycle(){
+
+	}
+
+	public void ShowAndroidToastMessage(){
+
+	}
+
+}
