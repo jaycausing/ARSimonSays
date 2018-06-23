6cee12d0dc643cab028b5b577146d0a195955d19
Jay Causing <jac84@njit.edu>
2018-06-23 11:15:30 -0400

    tracking works, have been setting plane to render at (0,0,0)

---
+++ Assets/_PROJECT/Scripts/ARCoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ARCoreManager : MonoBehaviour {
+
+	public ARCoreUtils utils;
+
+	private List<DetectedPlane> l_Planes = new List<DetectedPlane>();
+
+	// Use this for initialization
+	void Start () {
+		utils.QuitOnConnectionError();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		utils.UpdateAppLifecycle();
+
+		if(Session.Status == SessionStatus.NotTracking){
+			Debug.Log("Not tracking");
+			return;
+		} else {
+			utils.TrackPlanes(l_Planes);
+		}
+
+		Touch touch;
+
+		if(Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+			return;
+		
+		TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+		TrackableHitFlags.FeaturePointWithSurfaceNormal;
+		
+		if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit)){
+			if (hit.Trackable is DetectedPlane){
+				Debug.Log("Plane is detected and tracking!");
+			}
+		}
+
+	}
+}
--- Assets/_PROJECT/Scripts/ARCoreUtils.cs
+++ Assets/_PROJECT/Scripts/ARCoreUtils.cs
@@ -1,27 +1,70 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+	using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
 
 /*
 * Custom library for handling core ARCore functionality
-* 
+* Add to ARCoreManager GameObject
  */
 public class ARCoreUtils : MonoBehaviour {
 
-	public void TrackPlanes(List<DetectedPlane> l_Planes){
+	// Tracking Plane prefab
+	public GameObject trackingPlane;
+	// ARCore Session UI GameObject
+	public GameObject sessionUI;
 
+	public void TrackPlanes(List<DetectedPlane> l_Planes){
+		Session.GetTrackables<DetectedPlane>(l_Planes, TrackableQueryFilter.New);
+		bool showSearchingUI = true;
+        for (int i = 0; i < l_Planes.Count; i++){
+            if (l_Planes[i].TrackingState == TrackingState.Tracking){
+				//instantiate plane viz
+				GameObject planeObj = Instantiate(trackingPlane, Vector3.zero,
+				Quaternion.identity, transform);
+				
+                showSearchingUI = false;
+                break;
+            }
+			//TODO: remove searching text on track planes
+		}
 	}
 
-	public void QuitSessionOnAppError(){
+	public void QuitOnConnectionError(){
+		if(SessionStatusExtensions.IsError(Session.Status)){
+			SessionStatus status = Session.Status;
 
+			switch (status){
+				case SessionStatus.ErrorPermissionNotGranted:
+					AndroidShowToastMessage();
+					Application.Quit();
+					break;
+				case SessionStatus.ErrorSessionConfigurationNotSupported:
+					AndroidShowToastMessage();
+					Application.Quit();
+					break;
+				default:
+					AndroidShowToastMessage();
+					Application.Quit();
+					break;
+			}
+		}
 	}
 
 	public void UpdateAppLifecycle(){
-
+		if(Session.Status != SessionStatus.Tracking){
+			const int LOST_TRACKING_SLEEP_TIMEOUT = 15;
+			Screen.sleepTimeout = LOST_TRACKING_SLEEP_TIMEOUT;
+		} else {
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		}
 	}
 
-	public void ShowAndroidToastMessage(){
+	public void AndroidShowToastMessage(){
 
 	}
 
